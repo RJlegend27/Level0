f2fbfea7a30310724450809d6bb4acce97dd40d5
INeedAUniqueUsername <alexiscomical445@gmail.com>
2016-04-23 09:58:04 -0700

    turtle is life

---
+++ src/ObedTortoise.java
@@ -0,0 +1,49 @@
+import java.awt.Color;
+
+import org.teachingextensions.logo.Tortoise;
+
+public class ObedTortoise {
+	public static void main(String[] args) {
+		Tortoise.setSpeed(5);
+		Tortoise.setPenColor(Color.CYAN);
+		Tortoise.show();
+	
+		// We are going to make an obedient tortoise that will obey our commands to draw shapes. Please note that this
+		// is not a recipe, but a list step-by-step instructions.
+
+		// 1. Make a new class, create a main method, and show the tortoise.
+
+		// 2. Have the Tortoise draw a square.
+
+		// 3. Extract this code into a drawSquare() method.
+
+		// 4. Have the Tortoise draw a triangle.
+
+		// 5. Extract this code into a drawTriangle() method.
+
+		// 6. Have the Tortoise draw a circle.
+
+		// 7. Extract this code into a drawCircle() method.
+
+		// 8. Ask the user which shape they want. Draw the appropriate shape depending on their answer.
+
+		// 9. Ask the user which color they want. Color the the shape depending on their answer.
+	}
+static void drawTriangle(){
+	Tortoise.move(80);
+	Tortoise.turn(120);
+	Tortoise.move(80);
+	Tortoise.turn(120);
+	Tortoise.move(80);
+}
+
+		static void	drawSquare(){
+			Tortoise.move(50);
+			Tortoise.turn(90);
+			Tortoise.move(50);
+			Tortoise.turn(90);
+			Tortoise.move(50);
+			Tortoise.turn(90);
+			Tortoise.move(50);
+	}
+}
