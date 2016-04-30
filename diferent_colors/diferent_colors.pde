2657c11c7190743769f3360dc5aa44f58d97ddcf
INeedAUniqueUsername <alexiscomical445@gmail.com>
2016-04-30 09:59:17 -0700

    hi

---
+++ diferent_colors/diferent_colors.pde
@@ -0,0 +1,21 @@
+void setup() {
+  size(800, 800);
+}
+
+void draw() {
+  if(mousePressed == true) {
+    fill(255,255,4);
+  } else{
+    fill(4,255,255);
+    }
+  ellipse(10,10,50,50);
+
+   //2. make it a nice color
+
+   //3. if the mouse is pressed, fill the circle with a different color          
+
+   //1. draw an ellipse
+
+
+}
+// Copyright Wintriss Technical Schools 2013
--- src/ObedTortoise.java
+++ src/ObedTortoise.java
@@ -1,14 +1,26 @@
 import java.awt.Color;
 
+import javax.swing.JOptionPane;
+
 import org.teachingextensions.logo.Tortoise;
 
 public class ObedTortoise {
 	public static void main(String[] args) {
-		Tortoise.setSpeed(5);
+		Tortoise.setSpeed(10);
 		Tortoise.setPenColor(Color.CYAN);
 		Tortoise.show();
-	
-		// We are going to make an obedient tortoise that will obey our commands to draw shapes. Please note that this
+		String shape = JOptionPane.showInputDialog("What shape do you want?!");
+		if (shape.equals("triangle") ){
+			drawTriangle();
+		}
+		if (shape.equals("circle") ){
+			drawCircle();
+		}
+		if (shape.equals("square") ){
+			drawSquare();
+		}
+		// We are going to make an obedient tortoise that will obey our commands
+		// to draw shapes. Please note that this
 		// is not a recipe, but a list step-by-step instructions.
 
 		// 1. Make a new class, create a main method, and show the tortoise.
@@ -22,28 +34,38 @@ public class ObedTortoise {
 		// 5. Extract this code into a drawTriangle() method.
 
 		// 6. Have the Tortoise draw a circle.
-
+		drawCircle();
 		// 7. Extract this code into a drawCircle() method.
 
-		// 8. Ask the user which shape they want. Draw the appropriate shape depending on their answer.
+		// 8. Ask the user which shape they want. Draw the appropriate shape
+		// depending on their answer.
 
-		// 9. Ask the user which color they want. Color the the shape depending on their answer.
+		// 9. Ask the user which color they want. Color the the shape depending
+		// on their answer.
 	}
-static void drawTriangle(){
-	Tortoise.move(80);
-	Tortoise.turn(120);
-	Tortoise.move(80);
-	Tortoise.turn(120);
-	Tortoise.move(80);
-}
 
-		static void	drawSquare(){
-			Tortoise.move(50);
-			Tortoise.turn(90);
-			Tortoise.move(50);
-			Tortoise.turn(90);
-			Tortoise.move(50);
-			Tortoise.turn(90);
-			Tortoise.move(50);
+	static void drawTriangle() {
+		Tortoise.move(80);
+		Tortoise.turn(120);
+		Tortoise.move(80);
+		Tortoise.turn(120);
+		Tortoise.move(80);
+	}
+
+	static void drawCircle() {
+		for (int i = 0; i < 361; i++) {
+			Tortoise.move(1);
+			Tortoise.turn(1);
+		}
+	}
+
+	static void drawSquare() {
+		Tortoise.move(50);
+		Tortoise.turn(90);
+		Tortoise.move(50);
+		Tortoise.turn(90);
+		Tortoise.move(50);
+		Tortoise.turn(90);
+		Tortoise.move(50);
 	}
 }
