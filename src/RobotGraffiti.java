7c0bcf9f4fd32db6f395c87fbddfe2ee4fb6ab24
INeedAUniqueUsername <alexiscomical445@gmail.com>
2016-06-11 10:00:14 -0700

    robot.robot is life

---
+++ src/RobotGraffiti.java
@@ -0,0 +1,42 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotGraffiti {
+public static void main(String[] args) {
+	Robot robot = new Robot();
+	robot.setSpeed(12);
+	robot.penDown();
+	robot.move(200);
+	robot.turn(90);
+	robot.move(75);
+	robot.turn(90);
+	robot.move(75);
+	robot.turn(90);
+	robot.move(75);
+	robot.turn(90);
+	robot.turn(150);
+	robot.move(150);
+	robot.penUp();
+	robot.move(90);
+	robot.turn(720);
+	robot.move(10);
+	robot.turn(-720);
+	robot.move(50);
+	robot.turn(30);
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
