e5fbaa88564486e0f0c8188d58f5045bc6f424f5
INeedAUniqueUsername <alexiscomical445@gmail.com>
2016-07-16 10:02:53 -0700

    iyhdv

---
+++ src/factors.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class factors {
+	static void factor(int a, int i) {
+		if (a % i == 0) {
+			System.out.println(i + "is a fator of" + a);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		String num = JOptionPane.showInputDialog("Enter a *beep* *boop* number");
+		int a = Integer.parseInt(num);
+		for (int i = 1; i <= a; i++) {
+			factor(a, i);
+		}
+	}
+}
---
+++ src/sV.java
@@ -0,0 +1,13 @@
+
+public class sV {
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 0;
+		int z = a;
+		a = b;
+		b = z;
+		System.out.println("a is " + a);
+		System.out.println("b is " + b);
+		
+	}
+}
